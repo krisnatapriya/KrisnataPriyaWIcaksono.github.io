657b1e952b1cbb5b41fc23beab31329635e46cc5
krisnatapriya <124700164+krisnatapriya@users.noreply.github.com>
2024-02-21 00:28:51 +0700

    Update web.php

--- web.php
+++ web.php
@@ -13,9 +13,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//  127.0.0.1:8000/ ==> view welcome
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('mahasiswa', [MahasiswaController::class]);
+// //127.0.0.1:8000/Mahasiswa ==> <h1>Saya Mahasiswa</h1>
+// Route::get('/mahasiswa', function () {
+//     return "<h1>Saya Mahasiswa</h1>";
+// });
+
+// // 127.0.0.1:80000/Mahasiswa/1 ==> <h1>Saya Mahasiswa dengan Nim </h1>
+// Route::get('/mahasiswa/{nim}', function ($nim) {
+//     return "<h1>Saya Mahasiswa dengan nim $nim </h1>";
+// })->where('nim','[E][0-9]+');
+
+// Route::get('/mahasiswa/{nim}/{nama}', function ($nim, $nama) {
+//     return "<h1>Saya Mahasiswa dengan nim $nim & nama $nama </h1>";
+// })->where(['nim'=>'[E][0-9]+','nama'=>'[A-Za-z]+']);
+
+Route::get('mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('mahasiswa/{nim}/{nama}', [MahasiswaController::class, 'detail'])->where(['nim'=>'[E][0-9]+','nama'=>'[A-Za-z]+']);
